8c0d9c3d57a0a8b8af9ddbaa6eef3b4ed2bca472
Mingjie Jiang <jiang@mingjie.info>
2020-03-22 19:13:16 -0400

    Update

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,7 +5,7 @@ $.getJSON("/api/list", function(data) {
 		console.log(this);
 		$("#inventory").append(`\
             <tr id="row-${this._id}">\
-                <th>${this.name}</th>\
+                <th id="name-${this._id}">${this.name}</th>\
                 <td>${this.count}</td>\
                 <td id="actions-${this._id}">\
                 <a href="#!" class="button is-small is-rounded" onclick="update('${this._id}')">Update</a>\
@@ -50,18 +50,22 @@ function push(id) {
 }
 
 function destroy(id) {
-    var data = {
-        id: id
-    };
+    var r = confirm("Are you sure you want to delete " + $(`#name-${id}`).text() + "?");
 
-    $.ajax({
-		type: "POST",
-		url: "/api/delete",
-		data: data,
-		success: (res) => {
-            location.reload();
-        }
-	});
+    if (r) {
+        var data = {
+            id: id
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/api/delete",
+            data: data,
+            success: (res) => {
+                location.reload();
+            }
+        });
+    }
 }
 
 function create() {
